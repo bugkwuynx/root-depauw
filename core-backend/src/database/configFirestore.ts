c88e351890e88862853dbd91c8ad75b961ba682d
bugkwuynx <hngo_2028@depauw.edu>
2026-03-29 17:06:58 -0400

    refactor: improve Firestore initialization logic to check for existing apps

--- core-backend/src/database/configFirestore.ts
+++ core-backend/src/database/configFirestore.ts
@@ -1,10 +1,15 @@
-import {initializeApp, cert, type App} from 'firebase-admin/app';
+import {initializeApp, cert, type App, getApps, getApp} from 'firebase-admin/app';
 import {getFirestore, type Firestore} from 'firebase-admin/firestore';
 
 import serviceAccountCredentials from '../credentials/service-account-file.json' with {type: 'json'};
 
-const adminApp: App = initializeApp({
-    credential: cert(serviceAccountCredentials as any)
-});
+const intializeAppOptions = () => {
+    if (getApps.length === 0) {
+        return initializeApp({credential: cert(serviceAccountCredentials as any)});
+    }
+    return getApp();
+}
+
+const adminApp: App = intializeAppOptions();
 
 export const db: Firestore = getFirestore(adminApp);
