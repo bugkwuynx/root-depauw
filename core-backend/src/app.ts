fa515f7b99a6c997ee65f18048e4a97741decb72
minhkhuedoanthi <minhkhuedoanthi@gmail.com>
2026-04-04 00:18:06 -0400

    tweak notification

--- core-backend/src/app.ts
+++ core-backend/src/app.ts
@@ -1,5 +1,6 @@
 // src/app.ts
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 // Routes
@@ -8,8 +9,9 @@ import notificationsRouter from './routes/notifications.route.js';
 dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/notifications', notificationsRouter);
 
